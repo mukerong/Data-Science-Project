d3bc6ff7f31cbbe5e424a5a5b62f9c53cbc55bb5
Rachel Fu <guanrong.fu@gmail.com>
2017-08-01 08:35:30 -0500

    docs: write the structure for this analysis

---
+++ OSM Project - Chicago/OSM-Chicago.py
@@ -0,0 +1,29 @@
+import csv
+import codecs
+import pprint
+import re
+import xml.etree.cElementTree as ET
+import lxml
+import cerberus
+from collections import defaultdict
+
+# set-up the file and tag I am going to use
+osm_file = 'chicago_illinois.osm'
+sample_file = 'sample_chicago.osm'
+tag = ['node', 'way', 'relation']
+
+# find the elements from the original .osm file
+
+# write the generated elements into sample.osm file
+
+# explore the data
+
+# check the possible erros
+
+# solve the errors
+
+# write the sample file into a SQL database
+
+# query this dataset through SQL query
+
+# draw the conclusion
